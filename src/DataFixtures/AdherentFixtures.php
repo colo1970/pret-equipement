f865f2914d8cbe0035d0e474c3090356cb257268
colo1970 <o.daffe@yahoo.fr>
2022-10-27 14:45:36 +0200

    Ajout fixture Adherent

--- src/DataFixtures/AdherentFixtures.php
+++ src/DataFixtures/AdherentFixtures.php
@@ -19,11 +19,24 @@ class AdherentFixtures extends Fixture
     public function load(PersistenceObjectManager $manager): void
     {
         $adherent = new Adherent();
+        $adherent->setNom("Lepetit");
+        $adherent->setPrenom("Jean");
+        $adherent->setAdresseRue("54, Rue Lilas");
+        $adherent->setTelephone("0745779640");
+        $adherent->setCodePostal("69007");
+        $adherent->setVille("Lyon");
+        $adherent->setEMail(strtolower($adherent->getNom())."@gmail.com");
+        $adherent->setPassword($this->passwordHasher->hashPassword($adherent, $adherent->getNom()));
+        $manager->persist($adherent);  
+        $manager->flush(); 
+
+        $adherent = new Adherent();
         $adherent->setNom("Dupont");
         $adherent->setPrenom("Henri");
         $adherent->setAdresseRue("14 avenue de Foch");
         $adherent->setTelephone("0645789645");
         $adherent->setCodePostal("69001");
+        $adherent->setVille("Lyon");
         $adherent->setEMail(strtolower($adherent->getNom())."@gmail.com");
         $adherent->setPassword($this->passwordHasher->hashPassword($adherent, $adherent->getNom()));
         $manager->persist($adherent);  
@@ -36,6 +49,7 @@ class AdherentFixtures extends Fixture
         $adherentAdmin->setAdresseRue("81, Rue de Valmy");
         $adherentAdmin->setTelephone("0648776017");
         $adherentAdmin->setCodePostal("69009");
+        $adherent->setVille("Lyon");
         $adherentAdmin->setEMail("o.daffe@yahoo.fr");
         $adherentAdmin->setPassword($this->passwordHasher->hashPassword($adherentAdmin, $adherentAdmin->getNom()));
         $roleAdmin[] = Adherent::ROLE_ADMIN;
@@ -49,12 +63,13 @@ class AdherentFixtures extends Fixture
         $adherentManager->setAdresseRue("141, Rue de Republique");
         $adherentManager->setTelephone("0648778015");
         $adherentManager->setCodePostal("69001");
+        $adherent->setVille("Lyon");
         $adherentManager->setEMail("o.sofie@yahoo.fr");
         $adherentManager->setPassword($this->passwordHasher->hashPassword($adherentManager, $adherentManager->getNom()));     
         $roleManager[] = Adherent::ROLE_MANAGER;
         $adherentManager->setRoles($roleManager);
         $manager->persist($adherentManager);        
-        $manager->flush();   
+        $manager->flush();  
         
     }
 }
